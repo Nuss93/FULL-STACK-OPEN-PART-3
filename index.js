cdba140ba87ad685db326f7e588391c090615e3b
Nuss93 <nasreenrzk@gmail.com>
2020-11-13 00:25:09 +0800

    npm install

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.put('/api/persons/:id', (req,res) => {
     res.status(200).json({message:'ok', data:req.body})
 })
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`)
 })
